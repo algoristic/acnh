35f7f6dbbc595cad174b308bc2c537de8bfcc24d
leweke <male233@001NTB1066.ad.aconso.com>
2021-12-06 18:18:20 +0100

    Formatting

--- src/components/RemainingDomain.js
+++ src/components/RemainingDomain.js
@@ -1,8 +1,9 @@
 const RemainingDomain = ({ active, color, text, action }) => {
     const coloring = (active ? color : '#343a40');
     return (
-        <div className={'remaining-domain' + (active ? ' active' : '')} style={{ background: coloring }}
-            onClick={() => action()}>
+        <div onClick={() => action()}
+            className={'remaining-domain' + (active ? ' active' : '')}
+            style={{ background: coloring }}>
             { text }
         </div>
     );
